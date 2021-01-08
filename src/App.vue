306c16c6d3e052acbfed117cb442372577bbd948
Fabrício Guimarães <47043395+Fabricio-Guima@users.noreply.github.com>
2021-01-08 11:33:08 -0300

    initial commit
    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,137 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+<div id="app">
+    <header>
+        <h1>the<strong>Anime</strong>App</h1>
+
+        <form action="" class="search-box" @submit.prevent="HandleSearch">
+            <input type="search" v-model="search_query" class="search-field" placeholder="Procure por um anime..." required>
+        </form>
+    </header>
+    <main>
+      <div class="cards" v-if="animeList.length > 0">
+        <Card v-for="anime in animeList" :key="anime.mal_id" :anime="anime"/>
+      </div>
+     
+    </main>
+
+</div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
+import {ref} from 'vue'
+import Card from '@/components/Card'
+export default {
+  name: "App",
+  setup(){
+    const search_query= ref("")
+    const animeList = ref([]);
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+    const HandleSearch = async () => {
+      animeList.value = await fetch(`https://api.jikan.moe/v3/search/anime?q=${search_query.value}`)
+      .then(r => r.json())
+      .then(data => data.results);
 
-    &.router-link-exact-active {
-      color: #42b983;
+      search_query.value = "";
+     
+    }
+
+    return {
+      Card,
+      search_query,
+      animeList,
+      HandleSearch
     }
   }
+
+}
+</script>
+
+<style lang="scss">
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+    font-family: 'Fira Sans', sans-serif;
+}
+
+a {
+    text-decoration: none;
+
+}
+
+header {
+    padding-top: 50px;
+    padding-bottom: 50px;
+
+    h1 {
+        color: #888;
+        font-size: 42px;
+        font-weight: 400;
+        text-align: center;
+        text-transform: uppercase;
+        margin-bottom: 30px;
+
+        strong {
+            color: #313131;
+        }
+
+        &:hover {
+            color: #313131;
+        }
+    }
+
+    .search-box {
+        display: flex;
+        justify-content: center;
+        padding-left: 30px;
+        padding-right: 30px;
+
+        .search-field {
+          appearance: none;
+          background: none;
+          border: none;
+          outline: none;
+
+          background-color: #f3f3f3;
+          box-shadow: 0 4px 8px rgba(0, 0, 0, .15);
+
+          display: block;
+          width: 100%;
+          max-width: 600px;
+          padding: 15px;
+          border-radius: 8px;
+
+          color: #313131;
+          font-size: 20px;
+          transition: 0.3s;
+
+          &::placeholder {
+            color: #AAA;
+          }
+
+          &:focus, &valid {
+            color: #fff;
+            background-color: #313131;
+            box-shadow: 0px 0px 0px rgba(0, 0, 0, .15);
+          }
+        }
+    }
+}
+
+main {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding-left: 30px;
+  padding-right: 30px;
+
+  .cards {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0 -8px;
+  }
+
+  
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="card">
+      <a :href="anime.url" target="_blank">
+          <img :src="anime.image_url" alt="anime.title">
+          <h3>{{anime.title}}</h3>  
+      </a>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"Card",
+    props: ["anime"]
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+    flex: 1 1 33%;
+    width: 33%;
+    max-width: 33%;
+    padding: 0px 8px;
+    margin-bottom: 16px;
+
+    img {
+        width: 100%;
+        height: 300px;
+        object-fit: cover;
+        border-radius: 16px;
+        box-shadow: 0px 4px 8px rgba($color: #000000, $alpha: 0.15);
+        transition: 0.4s;
+    }
+
+    h3 {
+       padding: 16px;
+       color: #313131;
+       transition: 0.4s;
+    }
+
+    &:hover {
+        img {
+            transform: scale(1.05);
+        }
+
+        h3 {
+            color: #24E484;
+        }
+    }
+}
+    
+</style>
